b3406301833cf5109d5aa8ca26dbf6cbcd392390
EatGugbab <qaswq0530@hs.ac.kr>
2021-06-19 15:44:46 +0900

    git remote add origin https://github.com/EatGugbab/studyCodingTestWithPython.git git branch -M main git push -u origin main

---
+++ chapter4_#3.py
@@ -0,0 +1,44 @@
+# 게임 개발
+n, m = map(int, input().split())
+x, y, d = map(int, input().split())
+mapping = []
+for i in range(n):
+    mapping.append(list(map(int, input().split())))
+
+ans = 0
+cnt = 0
+
+# 북, 동, 남, 서
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+if mapping[x][y] != 1:
+    mapping[x][y] = 2
+    ans = 1
+
+while True:
+    d = (d - 1) % 4
+    nx = x + dx[d]
+    ny = y + dy[d]
+    if mapping[nx][ny] == 0:
+        mapping[nx][ny] = 2
+        x = nx
+        y = ny
+        ans += 1
+        cnt = 0
+    else:
+        cnt += 1
+
+    if cnt == 4:
+        d = (d - 2) % 4
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if mapping[nx][ny] != 1:
+            x = nx
+            y = ny
+            d = (d - 2) % 4
+            cnt = 0
+        else:
+            break
+
+print(ans)
